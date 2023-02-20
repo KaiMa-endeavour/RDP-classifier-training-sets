09e45ffab1afd6a059f7f69d27e9383f79c26c85
Kai Ma <107938829+KaiMa-endeavour@users.noreply.github.com>
2023-02-20 16:12:54 +0800

    Add files via upload

--- Rscript/03.fa_mod_Uniite2.R
+++ Rscript/03.fa_mod_Uniite2.R
@@ -3,37 +3,57 @@ library(tidyfst)
 library(stringr)
 library(doParallel)
 
+# output_path <- 'C:/Users/KaiMa/Desktop/xx'
 output_path <- '/clusterfs/node1/makai/Database/Unite2/modFun/'
-fa <- read_fasta('/clusterfs/node1/makai/Database/Unite2/Unite2.fasta')
+fa <- read_fasta('/clusterfs/node1/makai/Database/Unite2/UNITE_public_29.11.2022.fasta')
+# fa <- read_fasta('G:/UNITE_public_29.11.2022.fasta')
 nworker <- 62
 
+fa_fungi <- fa[which(grepl('|k__Fungi;', fa$Header)), ]
 
-fa_fungi <- fa[which(grepl('\\;k__Fungi\\;', fa$Header)), ]
 fa_fungi <- fa_fungi[!duplicated(fa_fungi$Sequence), ]
 header <- fa_fungi$Header
-header <- gsub('\tr__root;', ' ', header)
-header <- word(header, 1, -2, sep = ';')
-lineage <- word(header, 2, -1)
+lineage <- strsplit(header, '\\|') %>% sapply('[[', 2)
 
-ranks <- c('domain', 'phylum', 'class', 'order', 'family', 'genus')
-w <- c('k', 'p', 'c', 'o', 'f', 'g')
+ranks <- c('kingdom', 'phylum', 'class', 'order', 'family', 'genus', 'species')
+w <- c('k', 'p', 'c', 'o', 'f', 'g', 's')
+# header_table <- matrix('', nrow(fa_fungi), length(ranks))
 
+# tax <- matrix(NA, nrow(fa_fungi)*length(ranks), 5)
+
+# tax_id <- 1
 famod_item <- function(seq) {
   xx <- rep(12344321, length(ranks))
   s <- unlist(strsplit(lineage[seq], ';'))
   tax <- matrix(NA, length(s), 5)
   for (n in 1:length(s)) {
+    # n = 1
     if (strsplit(s[n], '__') %>% sapply('[', 1)  == w[n]) {
-      xx[n] <- paste(ifelse(n == 1, 'd', w[n]), (strsplit(s, '__') %>% sapply('[', 2))[n], sep = '__')
+      xx[n] <- paste(w[n], (strsplit(s, '__') %>% sapply('[', 2))[n], sep = '__')
       tax[n, ] <- c(sapply(strsplit(s[n], '__'), '[', 2), xx[n], ranks[n], match(ranks[n], ranks), lineage[seq])
     }
   }
   list(xx, as.data.table(tax))
 }
 
+# for (seq in 1:nrow(fa_fungi)) {
+#   # seq = 1
+#   
+#   for (n in 1:length(s)) {
+#     # n = 1
+#     if (strsplit(s[n], '__') %>% sapply('[', 1)  == w[n]) {
+#       header_table[seq, n] <- paste(ifelse(n == 1, 'd', w[n]), (strsplit(s, '__') %>% sapply('[', 2))[n], sep = '__')
+#       if(all(!grepl(paste0('^', header_table[seq, c(get('n'))], '$'), tax[, 2]))) {
+#         tax[tax_id, ] <- c(sapply(strsplit(s[n], '__'), '[', 2), header_table[seq, c(get('n'))], ranks[n], match(ranks[n], ranks), lineage[seq])
+#         tax_id <- tax_id + 1
+#       }
+#     }
+#   }
+#   cat('sequence', paste0(seq, '/', nrow(fa_fungi)), '----', header_table[seq, ], '\n')
+# }
 cl <- makeCluster(nworker, type = "PSOCK") # PSOCK (windows); FORK (linux);
 registerDoParallel(cl)
-header_out <- foreach(seq = 1:nrow(fa_fungi), .packages = c('stringr', 'tidyfst'), .verbose = T) %dopar% famod_item(seq)
+header_out <- foreach(seq = 1:nrow(fa_fungi), .packages = c('stringr', 'tidyfst')) %dopar% famod_item(seq)
 
 header_table <- t(sapply(1:length(header_out), function(i) {
   header_out[[i]][[1]]
@@ -54,8 +74,10 @@ if(!dir.exists(output_path)) dir.create(output_path)
 tax_add <- matrix(NA, nrow(header_table)*ncol(header_table), 5)
 tax_id <- 1
 for (i in 2:ncol(header_table)) {
+  # i=2
   item_c <- names(table(header_table[, c(get('i'))]))[names(table(header_table[, c(get('i'))])) != '']
   for (r in 1:length(item_c)) {
+    # r=26
     f_num <- table(header_table[which(header_table[, c(get('i'))] == item_c[r]), c(get('i')-1)])
     if (length(f_num) != 1) {
       header_table[, c(get('i'))] <- gsub(pattern = paste0('^', item_c[r], '$'), replacement = '', unlist(header_table[, c(get('i'))]))
@@ -70,6 +92,7 @@ for (i in 2:ncol(header_table)) {
     fathers <- header_table[, c(get('i'))-1]
     table(fathers)
     for (n in sons_id) {
+      # n = 69
       father_id <- which(fathers == header_table[n, c(get('i'))-1])
       if (all(header_table[father_id, c(get('i'))] == '') & all(header_table[n, c(get('i')):ncol(header_table)] == '')) {
         cat('Row ID:', n, 'ok! ', header_table[n, c(get('i'))-1], '\n')
@@ -87,12 +110,10 @@ colnames(tax_add) <- c('rank', 'class_rank', 'class', 'depth', 'tax')
 tax_add <- tax_add[which(rowSums(!is.na(tax_add)) > 0), ]
 tax_add <- tax_add[!duplicated(tax_add), ]
 tax <- rbind(tax, as.data.table(tax_add))
-fwrite(tax, paste0(output_path, 'tax_Unite2.txt'), sep = '\t')
+fwrite(tax, paste0(output_path, '/tax_UNITE_public_29.11.2022.txt'), sep = '\t')
 
 header <- sapply(1:length(header), function(n) paste(unlist(header_table[n, ]), collapse = ';'))
-header <- gsub('\\;+$', '', header)
-header <- gsub('\\;+', '\\;', header)
 
-fa_fungi$Header <- paste(word(fa_fungi$Header, 1, end = 1, sep = '\t'), header, sep = ' ')
-fwrite(data.table(`Seq ID` = word(fa_fungi$Header, 1, end = 1), header_table), paste0(output_path, 'header_table.csv'))
-write_fasta(fa_fungi, paste0(output_path, 'mod_Unite2.fa'))
+fa_fungi$Header <- paste(word(fa_fungi$Header, 1, end = 1, sep = '\\|'), header, sep = ' ')
+fwrite(data.table(`Seq ID` = word(fa_fungi$Header, 1, end = 1), header_table), paste0(output_path, '/header_table.csv'))
+write_fasta(fa_fungi, paste0(output_path, '/mod_UNITE_public_29.11.2022.fa'))
--- Rscript/04.taxid_rdp.R
+++ Rscript/04.taxid_rdp.R
@@ -1,13 +1,24 @@
-id_parent <- function(parent_name, file) {
-  tag_split <- as.data.table(do.call('rbind', strsplit(file[!is.na(file)], split = '\\*')))
-  if(parent_name %in% tag_split$V2) {
-    id_p <- tag_split[which(parent_name == tag_split$V2), ]$V1
+id_parent <- function(parent_name, file, depth) {
+  file <- file[file$V1 != 'xx']
+  if (any(grepl(paste0('\\*', parent_name, '\\*'), file$V1))) {
+    id_p <- unlist(strsplit(file[grepl(paste0('\\*', parent_name, '\\*'), file$V1) & grepl(depth, file$V1)]$V1, split = '\\*'))[1]
   } else {
     cat(parent_name, 'was not found.', '\n')
     break
   }
+  # tag_split <- as.data.table(do.call('rbind', strsplit(file[!is.na(file)], split = '\\*')))
+  # if(parent_name %in% tag_split$V2) {
+  #   id_p <- tag_split[which(parent_name == tag_split$V2), ]$V1
+  # } else {
+  #   cat(parent_name, 'was not found.', '\n')
+  #   break
+  # }
   id_p
 }
+# fasta <- './xx/mod_UNITE_public_29.11.2022.fa'
+# output_path = './xx/trainset_taxid_Fungi.txt'
+# ref_object <- 'Fungi'
+# nworker = 6
 taxid_rdp <- function(fasta, ref_object, nworker, output_path) {
   require(MicroEcoTk)
   require(tidyfst)
@@ -15,13 +26,17 @@ taxid_rdp <- function(fasta, ref_object, nworker, output_path) {
   require(parallel)
   options(warn = 2)
   fa <- read_fasta(fasta)
+  # rank_class <- data.table(rank = tax$V1 %>% strsplit(';') %>% lapply(rev) %>% sapply('[', 1), calss = tax$V3, depth = tax$V1 %>% strsplit(';') %>% sapply(length))
   header <- word(fa$Header, 2, -1, sep = ' ')
   items <- strsplit(header, ';')
   
-  if (ref_object %in% c('Bacteria', 'Archaea', 'Fungi')) {
+  if (ref_object %in% c('Bacteria', 'Archaea')) {
     ranks <- c('domain', 'phylum', 'class', 'order', 'family', 'genus')
   } else if (ref_object %in% 'Eukaryota') {
     ranks <- c('domain', 'kingdom', 'phylum', 'class', 'order', 'family', 'genus')
+    # ranks <- c('domain', 'superkingdom', 'kingdom', 'subkingdom', 'superphylum', 'phylum', 'subphylum', 'infraphylum', 'superclass', 'class', 'subclass', 'infraclass', 'superorder', 'order', 'suborder', 'superfamily', 'family', 'subfamily', 'genus')
+  } else if (ref_object %in% 'Fungi') {
+    ranks <- c('kingdom', 'phylum', 'class', 'order', 'family', 'genus', 'species')
   }
   ranks_depth <- data.table(rank = ranks, depth = 1:length(ranks)-1)
   
@@ -29,14 +44,17 @@ taxid_rdp <- function(fasta, ref_object, nworker, output_path) {
   items_table <- parLapply(cl, parLapply(cl, items, t), as.data.table) %>% rbindlist(fill = T)
   stopCluster(cl)
   colnames(items_table) <- ranks
+  # items_table <- items_table[1:3145]
   
   i = 1
-  file <- matrix(NA, sum(sapply(items, length)), 1)
+  file <- matrix('xx', length(unique(unlist(items)))*2, 1) %>% as.data.table()
   for (r in 1:length(ranks)) {
+    # r = 7
     sons <- items_table[, c(get('r'))] %>% table() %>% names()
     rank <- ranks[r]
     
     for (n in 1:length(sons)) {
+      # n = 805
       son <- sons[n]
       if (!is.na(son)) {
         son_l <- ifelse(grepl('\\(', son), gsub('\\(', '\\\\(', son), son)
@@ -45,30 +63,31 @@ taxid_rdp <- function(fasta, ref_object, nworker, output_path) {
         son_l <- ifelse(grepl('\\]', son), gsub('\\]', '\\\\]', son_l), son)
         
         if (r>1) father <- items_table[which(son == unlist(items_table[, c(get('r'))])), c(get('r'))-1] %>% table() %>% names() else father <- NULL
-        if(!son %in% c('d__Archaea', 'd__Bacteria', 'd__Eukaryota', 'd__Fungi')) id_p <- id_parent(parent_name = father, file) else id_p <- -1
+        if(!son %in% c('d__Archaea', 'd__Bacteria', 'd__Eukaryota', 'k__Fungi')) id_p <- id_parent(parent_name = father, file, depth =  ranks_depth$depth[ranks_depth$rank==rank]-1) else id_p <- -1
         if (r < length(ranks)) {
           son_after <- unlist(items_table[which(son == unlist(items_table[, c(get('r'))])), c(get('r')+1)])
           if (any(is.na(son_after))) {
-            if (!any(grepl(paste0('\\*', son_l, '\\*'), file) & grepl('\\*genus$', file))) {
-              file[i, ] <- paste(i-1, son, min(id_p), ranks_depth$depth[which(ranks_depth$rank == rank)], 'genus', sep = '*')
-              cat(file[i, ], '\n')
+            if (!any(grepl(paste0('\\*', son_l, '\\*'), file) & grepl(paste0('\\*', rev(ranks)[1], '$'), file))) {
+              file[i, ] <- paste(i-1, son, min(id_p), ranks_depth$depth[which(ranks_depth$rank == rank)], rev(ranks)[1], sep = '*')
+              cat(file[i, ]$V1, '\n')
               i <- i + 1
             }
           }
         }
-        if (length(son_after) > 0 & all(!is.na(son_after)) | (r == length(ranks))) {
+        if ((length(son_after) > 0 & all(!is.na(son_after))) | (r == length(ranks))) {
           if (!any(grepl(paste0('\\*', son_l, '\\*'), file) & grepl(paste0('\\*', rank, '$'), file))) {
             file[i, ] <- paste(i-1, son, min(id_p), ranks_depth$depth[which(ranks_depth$rank == rank)], rank, sep = '*')
-            cat(file[i, ], '\n')
+            cat(file[i, ]$V1, '\n')
             i <- i + 1
           }
         }
       }
     }
   }
-  file <- file[!is.na(file)]
-  file_non_redundant <- data.table(file[!duplicated(do.call('rbind', strsplit(file, '\\*'))[, -c(1, 3, 4)])])
-  if(length(file) == nrow(file_non_redundant)) cat('\n', 'Taxid non redundancy.', '\n')
+  file <- file[file$V1 != 'xx']
+  file_non_redundant <- data.table(file[!duplicated(do.call('rbind', strsplit(file$V1, '\\*'))[, -c(1, 3, 4)])])
+  if(length(file$V1) == nrow(file_non_redundant)) cat('\n', 'Taxid non redundancy.', '\n')
+  # fwrite(data.table(file), output_path, col.names = F)
   fwrite(file_non_redundant, output_path, col.names = F)
 }
 
